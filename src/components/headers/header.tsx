fe37cafe283d9690bd8d67ee07ec4f5862daf384
Malenkov DM <m18dm.dm@gmail.com>
2021-10-04 10:14:48 +0300

    create sicial network

---
+++ src/components/headers/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Headers() {
+    return(
+        <header>
+            <a href="#">Menu</a>
+            <a href="#">Users</a>
+            <a href="#">Messages</a>
+        </header>
+    );
+}
+
+export default Headers;

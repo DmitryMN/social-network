94669e1c01bee7b7daa744aa674b7bcc9cec4546
MalenkovDN <m18dm.dm@gmail.com>
2021-10-10 12:52:13 +0300

    create profile

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import HeaderContainer from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import './App.css';
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
       <div className="App">
           <HeaderContainer/>
-          <div className="navbar_container">
+          <div className="main_wrapper">
               <Navbar/>
+              <Profile/>
           </div>
       </div>
   );
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./profile.css";
+
+function Profile() {
+    return(
+        <div className="profile_wrapper">
+            <div className="logo">
+            </div>
+            <div>It's my first post</div>
+            <div>Post about the car</div>
+            <div>My little post about wheels</div>
+            <div>Engines cars</div>
+        </div>
+    );
+}
+
+export default Profile;

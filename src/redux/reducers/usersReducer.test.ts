665028691036e1565f3e4499e96f5b4d30030608
MalenkovDN <m18dm.dm@gmail.com>
2022-01-10 22:35:37 +0300

    refactoring usersReducer.test

--- src/redux/reducers/usersReducer.test.ts
+++ src/redux/reducers/usersReducer.test.ts
@@ -4,13 +4,15 @@ test("correct follow user", () => {
     //data
     const initialState: InitialStateType= {
         users: [
-            {id: 1, followed: true, fullName: "Dmitrii", status: "I am a developer", location: {city: "Moscow", country: "Russia"}},
-            {id: 2, followed: true, fullName: "Alex", status: "I am a doctor", location: {city: "Minsk", country: "Belarus"}},
-            {id: 3, followed: false, fullName: "Boris", status: "I am a manager", location: {city: "Kiev", country: "Ukraine"}},
-            {id: 4, followed: false, fullName: "Masha", status: "I am a student", location: {city: "Moscow", country: "Russia"}},
-        ]
+            {id: 1, followed: true, name: "Dmitrii", status: "I am a developer"},
+            {id: 2, followed: true, name: "Alex", status: "I am a doctor"},
+            {id: 3, followed: false, name: "Boris", status: "I am a manager"},
+            {id: 4, followed: false, name: "Masha", status: "I am a student"},
+        ],
+        pageSize: 10,
+        totalUsersCount: 100,
+        currentPage: 1
     }
-    // action
     let result = usersReducer(initialState, followUnfolllowAC(2));
     //expect
     expect(result.users[1].followed).toBe(false);
@@ -20,22 +22,25 @@ test("correct set new users", () => {
     //data
     const initialState: InitialStateType= {
         users: [
-            {id: 1, followed: true, fullName: "Dmitrii", status: "I am a developer", location: {city: "Moscow", country: "Russia"}},
-            {id: 2, followed: true, fullName: "Alex", status: "I am a doctor", location: {city: "Minsk", country: "Belarus"}},
-            {id: 3, followed: false, fullName: "Boris", status: "I am a manager", location: {city: "Kiev", country: "Ukraine"}},
-            {id: 4, followed: false, fullName: "Masha", status: "I am a student", location: {city: "Moscow", country: "Russia"}},
-        ]
+            {id: 1, followed: true, name: "Dmitrii", status: "I am a developer"},
+            {id: 2, followed: true, name: "Alex", status: "I am a doctor"},
+            {id: 3, followed: false, name: "Boris", status: "I am a manager"},
+            {id: 4, followed: false, name: "Masha", status: "I am a student"},
+        ],
+        pageSize: 10,
+        totalUsersCount: 100,
+        currentPage: 1
     }
 
     const newUsers: arrUsersType = [
-        {id: 5, followed: true, fullName: "Ann", status: "I am a engineer", location: {city: "Moscow", country: "Russia"}},
-        {id: 6, followed: true, fullName: "Bob", status: "I am a doctor", location: {city: "Minsk", country: "Belarus"}},
+        {id: 5, followed: true, name: "Ann", status: "I am a engineer"},
+        {id: 6, followed: true, name: "Bob", status: "I am a doctor"},
     ];
 
     // action
     let result = usersReducer(initialState, setUsersAC(newUsers));
     //expect
     expect(result.users.length).toBe(6);
-    expect(result.users[5].fullName).toBe("Bob");
+    expect(result.users[5].name).toBe("Bob");
 })
 

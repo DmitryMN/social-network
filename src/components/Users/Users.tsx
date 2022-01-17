73ecf594e519b12cb19b4008c5ba96036cd0acf3
MalenkovDN <m18dm.dm@gmail.com>
2022-01-17 23:10:18 +0300

    add UsersApiComponent and refactoring Users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,52 +1,45 @@
 import React from "react";
-import {UsersPropsType} from "./UsersContainer";
 import User from "./user/User";
 import "./users.css";
-import axios from "axios";
+import {InitialStateType} from "../../redux/reducers/usersReducer";
 
-class Users extends React.Component<UsersPropsType> {
+type UsersPropsType = {
+    users: InitialStateType
+    onChangeFollowUnfollow: (id: number) => void
+    setCurrentPage: (currentPage: number) => void
+}
 
-    constructor(props: UsersPropsType) {
-        super(props);
+const Users = (props: UsersPropsType) => {
+
+    const setCurrentPageHandler = (currentPage: number) => {
+        props.setCurrentPage(currentPage);
     }
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.users.pageSize}&page=${this.props.users.currentPage}`).then(
-            response => {
-                this.props.setNewUsers(response.data.items);
-            }
-        );
+    let pagesCount = Math.ceil(props.users.totalUsersCount / props.users.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    setCurrentPageHandler(currentPage: number) {
-        this.props.setCurrentPage(currentPage);
-    }
-
-    render() {
-        let pagesCount = Math.ceil(this.props.users.totalUsersCount / this.props.users.pageSize);
-        let pages = [];
-        for(let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        return(
-            <div className="users_page">
-                <p>Users:</p>
-                <div className="users_container">
-                    <div>
-                        {pages.map(page => <span className={this.props.users.currentPage === page ? "selected" : ""}
-                                                 onClick={ () => {this.setCurrentPageHandler(page)}}>{page}</span>)}
-                    </div>
-                    {
-                        this.props.users.users.map(user => <User key={user.id} id={user.id}
-                                                            followed={user.followed} name={user.name}
-                                                            status={user.status}
-                                                            onChangeFollowUnfollow={this.props.onChangeFollowUnfollow} />)
-                    }
+    return (
+        <div className="users_page">
+            <p>Users:</p>
+            <div className="users_container">
+                <div>
+                    {pages.map(page => <span className={props.users.currentPage === page ? "selected" : ""}
+                                             onClick={() => {
+                                                 setCurrentPageHandler(page)
+                                             }}>{page}</span>)}
                 </div>
+                {
+                    props.users.users.map(user => <User key={user.id} id={user.id}
+                                                        followed={user.followed} name={user.name}
+                                                        status={user.status}
+                                                        onChangeFollowUnfollow={props.onChangeFollowUnfollow}/>)
+                }
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Users;
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -4,6 +4,7 @@ import {rootReducerType} from "../../redux/store/redux_store";
 import {Dispatch} from "redux";
 import {connect} from "react-redux";
 import Users from "./Users";
+import axios from "axios";
 
 type MapStateToPropsType = {
     users: InitialStateType
@@ -15,7 +16,28 @@ export type MapDispatchToPropsType = {
     setCurrentPage: (currentPage: number) => void
 }
 
-export type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType;
+export type UsersApiComponentType = MapStateToPropsType & MapDispatchToPropsType;
+
+
+class UsersApiComponent extends React.Component<UsersApiComponentType> {
+    constructor(props: UsersApiComponentType) {
+        super(props);
+    }
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.users.pageSize}&page=${this.props.users.currentPage}`).then(
+            response => {
+                this.props.setNewUsers(response.data.items);
+            }
+        );
+    }
+
+    render() {
+        return (
+            <Users users={this.props.users} onChangeFollowUnfollow={this.props.onChangeFollowUnfollow} setCurrentPage={this.props.setCurrentPage} />
+        );
+    }
+}
 
 const mapStateToProps = (state: rootReducerType): MapStateToPropsType => {
     return {
@@ -37,4 +59,4 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType  => {
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent);

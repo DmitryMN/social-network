2a77fc3a687298606ed9d0c6f7e9cbe1d14c1578
MalenkovDN <m18dm.dm@gmail.com>
2022-04-14 14:21:03 +0300

    use useState in ProfileStatus component

--- src/components/profile/UserProfile/ProfileStatus.tsx
+++ src/components/profile/UserProfile/ProfileStatus.tsx
@@ -1,15 +1,36 @@
+import { type } from "os";
 import React from "react";
 
-export const ProfileStatus = (props: any) => {
-    return (
-        <div>
-            <div>
-                <span>{props.status}</span>
-            </div>
-            <div>
-                <input value={props.status} />
-            </div>
-        </div>
+type ProfileStatusPropsType = {
+    status: string
+}
 
-    );
+interface ProfileStatusType {
+    editMode: boolean
+}
+
+
+export class ProfileStatus extends React.Component<ProfileStatusPropsType, ProfileStatusType> {
+    constructor(props: ProfileStatusPropsType) {
+        super(props);
+        this.state = {
+            editMode: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.editMode ?
+                        <div>
+                            <span>{this.props.status}</span>
+                        </div> :
+                        <div>
+                            <input value={this.props.status} />
+                        </div>
+                }
+            </div>
+        );
+    }
 }
--- src/components/profile/UserProfile/UserProfile.tsx
+++ src/components/profile/UserProfile/UserProfile.tsx
@@ -3,6 +3,7 @@ import { ProfileUserNullType } from "../ProfileUserContainer";
 import Preloader from "../../preloader/Preloader";
 import { ProfileStatus } from "./ProfileStatus";
 
+
 export const UserProfile = ({profiles}: ProfileUserNullType) => {
     if(!profiles) {
         return <Preloader />
@@ -11,7 +12,7 @@ export const UserProfile = ({profiles}: ProfileUserNullType) => {
         <div className="user_profile">
             <div className="logo">
             </div>
-            <ProfileStatus status="I am frontend developer"/>
+            <ProfileStatus status="I am frontend developer" />
             <div>Full Name: {profiles.fullName}</div>
             <div>About me: {profiles.aboutMe ? profiles.aboutMe : "is empty"}</div>
         </div>
